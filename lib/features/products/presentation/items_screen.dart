f2f2e1c40991c8a6aecac48d2b8ca11ae019be1f
Aditya Mittal <adityamittal0918@gmail.com>
2024-01-28 19:12:19 +0530

    .

--- lib/features/products/presentation/items_screen.dart
+++ lib/features/products/presentation/items_screen.dart
@@ -165,161 +165,160 @@ class _ItemsScreenState extends ConsumerState<ItemsScreen> {
           elevation: 0.0,
           scrolledUnderElevation: 0.0,
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      bottomSheet(context);
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: lightPrimary),
-                      ),
-                      child: Row(
-                        children: [
-                          SvgPicture.asset('assets/icons/sort.svg'),
-                          const SizedBox(
-                            width: 8,
+        body: products.when(
+          data: (data) {
+            List<Product> filteredList = data;
+            if (filters.category != null) {
+              filteredList = filteredList
+                  .where((product) => product.category.name == filters.category)
+                  .toList();
+            }
+            if (filters.isVeg != null) {
+              if (filters.isVeg == true) {
+                filteredList =
+                    filteredList.where((product) => product.isVeg).toList();
+              }
+            }
+            if (filters.priceSort != null) {
+              if (filters.priceSort == SortState.asc) {
+                filteredList.sort((a, b) => a.price.compareTo(b.price));
+              } else {
+                filteredList.sort((a, b) => b.price.compareTo(a.price));
+              }
+            }
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          bottomSheet(context);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: lightPrimary),
                           ),
-                          const Text(
-                            'Sort',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          const Icon(
-                            Icons.arrow_drop_down_sharp,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  GestureDetector(
-                    child: Container(
-                      height: 36,
-                      width: w * 0.416,
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: lightPrimary),
-                      ),
-                      child: DropdownButton<String>(
-                        isExpanded: true,
-                        hint: const Text(
-                          'Type',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                          child: Row(
+                            children: [
+                              SvgPicture.asset('assets/icons/sort.svg'),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              const Text(
+                                'Sort',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              const Icon(
+                                Icons.arrow_drop_down_sharp,
+                              ),
+                            ],
                           ),
                         ),
-                        underline: const SizedBox(),
-                        items: categoryList.map(buildMenuItem).toList(),
-                        value: filters.category,
-                        onChanged: (value) => setState(
-                          () {
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      GestureDetector(
+                        child: Container(
+                          height: 36,
+                          width: w * 0.416,
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: lightPrimary),
+                          ),
+                          child: DropdownButton<String>(
+                            isExpanded: true,
+                            hint: const Text(
+                              'Type',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            underline: const SizedBox(),
+                            items: categoryList.map(buildMenuItem).toList(),
+                            value: filters.category,
+                            onChanged: (value) => setState(
+                              () {
+                                ref.read(filterProvider.notifier).update(
+                                      (state) => Filters(
+                                        category: value,
+                                        priceSort: state.priceSort,
+                                        ratingSort: state.ratingSort,
+                                        isVeg: state.isVeg,
+                                      ),
+                                    );
+                              },
+                            ),
+                            icon: const Icon(Icons.arrow_drop_down),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (filters.isVeg == null || filters.isVeg == false) {
                             ref.read(filterProvider.notifier).update(
                                   (state) => Filters(
-                                    category: value,
+                                    category: state.category,
                                     priceSort: state.priceSort,
                                     ratingSort: state.ratingSort,
-                                    isVeg: state.isVeg,
+                                    isVeg: true,
                                   ),
                                 );
-                          },
-                        ),
-                        icon: const Icon(Icons.arrow_drop_down),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      if (filters.isVeg == null || filters.isVeg == false) {
-                        ref.read(filterProvider.notifier).update(
-                              (state) => Filters(
-                                category: state.category,
-                                priceSort: state.priceSort,
-                                ratingSort: state.ratingSort,
-                                isVeg: true,
-                              ),
-                            );
-                      } else {
-                        ref.read(filterProvider.notifier).update(
-                              (state) => Filters(
-                                category: state.category,
-                                priceSort: state.priceSort,
-                                ratingSort: state.ratingSort,
-                                isVeg: null,
-                              ),
-                            );
-                      }
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(7),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          border: Border.all(color: lightPrimary),
-                          color: filters.isVeg == true
-                              ? lightPrimary
-                              : Colors.white),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Veg',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color:
-                                  filters.isVeg != null && filters.isVeg == true
+                          } else {
+                            ref.read(filterProvider.notifier).update(
+                                  (state) => Filters(
+                                    category: state.category,
+                                    priceSort: state.priceSort,
+                                    ratingSort: state.ratingSort,
+                                    isVeg: null,
+                                  ),
+                                );
+                          }
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(7),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(color: lightPrimary),
+                              color: filters.isVeg == true
+                                  ? lightPrimary
+                                  : Colors.white),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Veg',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: filters.isVeg != null &&
+                                          filters.isVeg == true
                                       ? Colors.white
                                       : Colors.black,
-                            ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            products.when(
-              data: (data) {
-                List<Product> filteredList = data;
-                if (filters.category != null) {
-                  filteredList = filteredList
-                      .where((product) =>
-                          product.category.name == filters.category)
-                      .toList();
-                }
-                if (filters.isVeg != null) {
-                  if (filters.isVeg == true) {
-                    filteredList =
-                        filteredList.where((product) => product.isVeg).toList();
-                  }
-                }
-                if (filters.priceSort != null) {
-                  if (filters.priceSort == SortState.asc) {
-                    filteredList.sort((a, b) => a.price.compareTo(b.price));
-                  } else {
-                    filteredList.sort((a, b) => b.price.compareTo(a.price));
-                  }
-                }
-                return filteredList.isEmpty
+                ),
+                filteredList.isEmpty
                     ? const Expanded(
                         child: Center(
                           child:
@@ -343,22 +342,22 @@ class _ItemsScreenState extends ConsumerState<ItemsScreen> {
                             );
                           },
                         ),
-                      );
-              },
-              error: (error, stackTrace) {
-                return Center(
-                  child: Text(
-                    error.toString(),
-                  ),
-                );
-              },
-              loading: () {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            )
-          ],
+                      ),
+              ],
+            );
+          },
+          loading: () {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+          error: (error, stackTrace) {
+            return Center(
+              child: Text(
+                error.toString(),
+              ),
+            );
+          },
         ),
       ),
     );
